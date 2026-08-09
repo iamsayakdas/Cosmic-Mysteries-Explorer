42b72631ac26cc8680555499f09d38ad81efb70d
iamsayakdas <myselfsayakdas@gmail.com>
2026-08-10 03:14:54 +0530

    Sprint 2: Complete Solar System planet textures

--- frontend/src/scenes/Universe/celestial/Neptune.jsx
+++ frontend/src/scenes/Universe/celestial/Neptune.jsx
@@ -1,12 +1,17 @@
 import { useFrame } from "@react-three/fiber";
 import { useRef, useState } from "react";
+import { useTexture } from "@react-three/drei";
+
 import PlanetLabel from "./PlanetLabel";
+import neptuneTexture from "../../../assets/textures/neptune/neptune.jpg";
 
 export default function Neptune() {
   const orbitRef = useRef();
   const planetRef = useRef();
 
   const [hovered, setHovered] = useState(false);
+
+  const texture = useTexture(neptuneTexture);
 
   useFrame(({ clock }) => {
     const t = clock.getElapsedTime();
@@ -35,11 +40,13 @@ export default function Neptune() {
           setHovered(false);
         }}
       >
-        <sphereGeometry args={[0.40, 48, 48]} />
+        <sphereGeometry args={[0.40, 64, 64]} />
 
         <meshStandardMaterial
-          color="#4169E1"
-          roughness={0.85}
+          map={texture}
+          color="#ffffff"
+          roughness={1}
+          metalness={0}
         />
 
         <PlanetLabel
