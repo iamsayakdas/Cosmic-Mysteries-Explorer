a876604cd90cc32b70f2f273282e447d45f0e30f
iamsayakdas <myselfsayakdas@gmail.com>
2026-08-10 02:47:29 +0530

    Sprint 2: Add Mars surface texture

--- frontend/src/scenes/Universe/celestial/Mars.jsx
+++ frontend/src/scenes/Universe/celestial/Mars.jsx
@@ -1,12 +1,17 @@
 import { useFrame } from "@react-three/fiber";
 import { useRef, useState } from "react";
+import { useTexture } from "@react-three/drei";
+
 import PlanetLabel from "./PlanetLabel";
+import marsTexture from "../../../assets/textures/mars/mars.jpg";
 
 export default function Mars() {
   const orbitRef = useRef();
   const planetRef = useRef();
 
   const [hovered, setHovered] = useState(false);
+
+  const texture = useTexture(marsTexture);
 
   useFrame(({ clock }) => {
     const t = clock.getElapsedTime();
@@ -35,11 +40,13 @@ export default function Mars() {
           setHovered(false);
         }}
       >
-        <sphereGeometry args={[0.24, 48, 48]} />
+        <sphereGeometry args={[0.24, 64, 64]} />
 
         <meshStandardMaterial
-          color="#B55239"
-          roughness={0.95}
+          map={texture}
+          color="#ffffff"
+          roughness={1}
+          metalness={0}
         />
 
         <PlanetLabel
