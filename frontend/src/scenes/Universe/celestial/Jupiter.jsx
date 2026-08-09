8c6dc6cd334bca8938532caaf3f7be352b140698
iamsayakdas <myselfsayakdas@gmail.com>
2026-08-10 02:51:00 +0530

    Sprint 2: Add Jupiter surface texture

--- frontend/src/scenes/Universe/celestial/Jupiter.jsx
+++ frontend/src/scenes/Universe/celestial/Jupiter.jsx
@@ -1,12 +1,17 @@
 import { useFrame } from "@react-three/fiber";
 import { useRef, useState } from "react";
+import { useTexture } from "@react-three/drei";
+
 import PlanetLabel from "./PlanetLabel";
+import jupiterTexture from "../../../assets/textures/jupiter/jupiter.jpg";
 
 export default function Jupiter() {
   const orbitRef = useRef();
   const planetRef = useRef();
 
   const [hovered, setHovered] = useState(false);
+
+  const texture = useTexture(jupiterTexture);
 
   useFrame(({ clock }) => {
     const t = clock.getElapsedTime();
@@ -38,8 +43,10 @@ export default function Jupiter() {
         <sphereGeometry args={[0.75, 64, 64]} />
 
         <meshStandardMaterial
-          color="#C9A77A"
-          roughness={0.9}
+          map={texture}
+          color="#ffffff"
+          roughness={1}
+          metalness={0}
         />
 
         <PlanetLabel
